84a962c44d5bf863720373eb844e46d24d662fa2
Tuong Pham <tuongpvth@gmail.com>
2025-12-13 23:09:41 +0700

    check supported market

--- crates/quick_common/src/market.rs
+++ crates/quick_common/src/market.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-use crate::types::{ExchangeId, InnerMarketId, MarketName};
+use crate::types::{InnerMarketId, MarketName};
 
 pub struct MarketMapping {
     pub markets_by_name: HashMap<MarketName, InnerMarketId>,
@@ -8,18 +8,19 @@ pub struct MarketMapping {
 }
 
 impl MarketMapping {
-    pub fn new(_exchange_id: ExchangeId) -> Self {
-        Self {
-            markets_by_name: HashMap::new(),
-            markets_by_id: HashMap::new(),
-        }
-    }
-
     pub fn map_to_id(&self, name: &MarketName) -> InnerMarketId {
         *self.markets_by_name.get(name).unwrap()
     }
 
     pub fn map_to_name(&self, id: &InnerMarketId) -> MarketName {
         self.markets_by_id.get(id).unwrap().clone()
+    }
+
+    pub fn is_market_name_supported(&self, name: &MarketName) -> bool {
+        self.markets_by_name.contains_key(name)
+    }
+
+     pub fn is_market_id_supported(&self, id: &InnerMarketId) -> bool {
+        self.markets_by_id.contains_key(id)
     }
 }
